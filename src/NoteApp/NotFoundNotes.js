0b2baed7b4bb7eed43d29a1175a2a67903609c06
github.arief <78076941+arief-github@users.noreply.github.com>
2022-07-07 22:47:51 +0700

    adding icons and styling page

--- src/NoteApp/NotFoundNotes.js
+++ src/NoteApp/NotFoundNotes.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { ArchiveIcon } from '@heroicons/react/solid';
 
 const NotesNotFound = () => {
 	return (
 		<div className="notes-list__empty-message">
-			<p>Not Found or Empty</p>
+			<ArchiveIcon className="icon"/> <p>Not Found or Empty</p>
 		</div>
 	)
 }
